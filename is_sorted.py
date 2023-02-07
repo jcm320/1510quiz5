e2619b25bd984fb76d548fa2144ef4e0dfb39d6e
dndannynguyen <dn.dannynguyen@hotmail.com>
2023-02-07 14:34:01 -0800

    Rework on the docstrings for is_sorted function

--- is_sorted.py
+++ is_sorted.py
@@ -3,28 +3,33 @@ def is_sorted(integer_list):
     """
     Check if the integer_list is non-decreasing sorted.
 
-    A function that checks if the integer_list is sorted in non-decreasing order,
+    A function that checks if the integer_list is sorted in non-decreasing order.
 
-    :param integer_list:
-    :return:
+    :param integer_list: a list of integers.
+    :pre-condition: a list of integers that is in non-deceasing order (possibly-empty).
+    :post-condition: define the non-decreasing sorted list correctly, and check on integer_list.
 
-    >>>integer_list = [1,2,3]
+    :return: A boolean value displaying True if integer_list is sorted in non-decreasing order, and False if not.
+
+    >>> is_sorted([1,2,3])
     True
-    >>>integer_list = [3,2,1]
+    >>> is_sorted([3,2,1])
     False
-    >>>integer_list = []
+    >>> is_sorted([])
     True
-    >>>integer_list = [4,6,5,9,4,8]
+    >>> is_sorted([9,1,-3,7,4,3,7])
     False
+    >>> is_sorted([-4,-2,-1,3,3,4,4,4,7,8])
+    True
+
     """
     another_list = sorted(integer_list)
-
     if integer_list == another_list:
         return True
     else:
         return False
 
-def main():
 
-if __name__ == "__main__":
-    main()
+
+
+
